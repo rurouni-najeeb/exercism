a85f0ccdfe8b8b7fc7aec3bd24e764619c992f86
rurouni-najeeb <najeeb.khan96@gmail.com>
2023-02-07 13:50:36 +0100

    robot-name in progress

--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,34 +1,89 @@
-import { generateKey } from "crypto";
-
 export class Robot {
   _name : string;
-  static nameSet : string[];
+  static characters : string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  static numbers : string = "0123456789";
+  static nameSet = new Set<string>();
+
+  static prefixList = Robot.generateCombinations(Robot.characters, 2);
+  static usedPrefix : string[] = [];
+
+  static suffixList = Robot.generateCombinations(Robot.numbers, 3);
+  static usedSuffix : string[] = [];
 
   constructor() {
-    let generatedName : string = Robot.generateName();
-    while(Robot.nameSet.includes(generatedName))
-      generatedName = Robot.generateName();
+    this._name = Robot.generateName(); 
+  }
+
+  static generateCombinations(feedString : string, length : number) :  string[]{
+
+    if(length < 1)
+      return [""];
     
-    this._name = generatedName; 
+    let combinations = Robot.generateCombinations(feedString, length - 1);
+    let newCombinations : string[] = [];
+    for(let i = 0; i < feedString.length; i++){
+      combinations.forEach((element) => {
+        newCombinations.push(feedString[i] + element);
+      });
+    };
+
+    return newCombinations;
   }
 
   public static generateName() : string {
-    let characters : string = "ABCDEFGHIJKLMNOPQRSTUVWX";
-    let inx1 : number = Math.ceil(Math.random() * 26) + 1;
-    let inx2 : number = Math.ceil(Math.random() * 26) + 1;
-    let alpha_part : string = characters[inx1] + characters[inx2];
-    let integer_part : number = Math.ceil(Math.random() * 10000);
-    return `${alpha_part}${integer_part}`
+    let prefix : string = "";
+    let suffix : string = "";
+    let generatedString : string = "";
+
+    if(Robot.prefixList.length > 0){
+      let inx : number = Math.ceil(Math.random() * (Robot.prefixList.length - 1));
+      prefix = Robot.prefixList[inx];
+      Robot.prefixList.splice(inx);
+      Robot.usedPrefix.push(prefix);
+    }
+    if(Robot.suffixList.length > 0){
+      let inx : number = Math.ceil(Math.random() * (Robot.suffixList.length - 1));
+      suffix = Robot.suffixList[inx];
+      Robot.suffixList.splice(inx);
+      Robot.usedPrefix.push(suffix);
+    }
+
+    if(prefix !== "" && suffix !== ""){
+      generatedString = `${prefix}${suffix}`;
+    }
+    if(prefix === "" && suffix !== ""){
+      while(generatedString !== "" && Robot.nameSet.has(generatedString)){
+        let inx : number = Math.ceil(Math.random() * (Robot.usedPrefix.length - 1));
+        generatedString = `${Robot.usedPrefix[inx]}${suffix}`;
+      }
+    }
+    if(prefix !== "" && suffix === ""){
+      while(generatedString !== "" && Robot.nameSet.has(generatedString)){
+        let inx : number = Math.ceil(Math.random() * (Robot.usedSuffix.length - 1));
+        generatedString = `${prefix}${Robot.usedSuffix[inx]}`;
+      }
+    }
+    if(prefix === "" && suffix === ""){
+      while(generatedString !== "" && Robot.nameSet.has(generatedString)){
+        let prefixInx : number = Math.ceil(Math.random() * (Robot.usedPrefix.length - 1));
+        let suffixInx : number = Math.ceil(Math.random() * (Robot.usedSuffix.length - 1))
+        generatedString = `${Robot.usedPrefix[prefixInx]}${Robot.usedSuffix[suffixInx]}`;
+      }
+    }
+
+    Robot.nameSet.add(generatedString);
+    return generatedString;
   }
+
   public get name(): string {
-    return this._name
+    return this._name;
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    this._name = Robot.generateName();
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    Robot.nameSet = new Set<string>();
   }
 }
