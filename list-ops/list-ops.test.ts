b32401eaed49abdb52eae7d8f65987813ba349b4
Najeeb Khan <najeeb.khan@moia.io>
2023-03-06 09:39:39 +0100

    implement foldl method

--- list-ops/list-ops.test.ts
+++ list-ops/list-ops.test.ts
@@ -1,10 +1,10 @@
-import { List } from './list-ops'
+import { List } from "./list-ops";
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
-      toHaveValues(...expected: unknown[]): CustomMatcherResult
+      toHaveValues(...expected: unknown[]): CustomMatcherResult;
     }
   }
 }
@@ -14,151 +14,151 @@ expect.extend({
     received: ReturnType<typeof List.create>,
     ...expected: unknown[]
   ): jest.CustomMatcherResult {
-    if (!('forEach' in received)) {
+    if (!("forEach" in received)) {
       return {
         pass: false,
         message: (): string => `Implement .forEach(callback) on your list`,
-      }
+      };
     }
 
-    const values: unknown[] = []
+    const values: unknown[] = [];
     received.forEach((item) => {
-      values.push(item)
-    })
+      values.push(item);
+    });
 
-    const pass = JSON.stringify(values) === JSON.stringify(expected)
+    const pass = JSON.stringify(values) === JSON.stringify(expected);
     return {
       pass,
       message: (): string =>
         pass
-          ? ''
+          ? ""
           : `Expected to see the following values: ${JSON.stringify(
               expected
             )}, actual: ${JSON.stringify(values)}`,
-    }
+    };
   },
-})
+});
 
-describe('append entries to a list and return the new list', () => {
-  it('empty lists', () => {
-    const list1 = List.create()
-    const list2 = List.create()
-    expect(list1.append(list2)).toEqual(List.create())
-  })
+describe("append entries to a list and return the new list", () => {
+  it("empty lists", () => {
+    const list1 = List.create();
+    const list2 = List.create();
+    expect(list1.append(list2)).toEqual(List.create());
+  });
 
-  it('empty list to list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    const list2 = List.create()
-    expect(list1.append(list2)).toEqual(list1)
-  })
+  it("empty list to list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    const list2 = List.create();
+    expect(list1.append(list2)).toEqual(list1);
+  });
 
-  it('non-empty lists', () => {
-    const list1 = List.create(1, 2)
-    const list2 = List.create(2, 3, 4, 5)
-    expect(list1.append(list2)).toHaveValues(1, 2, 2, 3, 4, 5)
-  })
-})
+  it("non-empty lists", () => {
+    const list1 = List.create(1, 2);
+    const list2 = List.create(2, 3, 4, 5);
+    expect(list1.append(list2)).toHaveValues(1, 2, 2, 3, 4, 5);
+  });
+});
 
-describe('concat lists and lists of lists into new list', () => {
-  it('empty list', () => {
-    const list1 = List.create()
-    const list2 = List.create()
-    expect(list1.concat(list2)).toHaveValues()
-  })
+describe("concat lists and lists of lists into new list", () => {
+  it("empty list", () => {
+    const list1 = List.create();
+    const list2 = List.create();
+    expect(list1.concat(list2)).toHaveValues();
+  });
 
-  it('list of lists', () => {
-    const list1 = List.create(1, 2)
-    const list2 = List.create(3)
-    const list3 = List.create()
-    const list4 = List.create(4, 5, 6)
-    const listOfLists = List.create(list2, list3, list4)
-    expect(list1.concat(listOfLists)).toHaveValues(1, 2, 3, 4, 5, 6)
-  })
-})
+  it("list of lists", () => {
+    const list1 = List.create(1, 2);
+    const list2 = List.create(3);
+    const list3 = List.create();
+    const list4 = List.create(4, 5, 6);
+    const listOfLists = List.create(list2, list3, list4);
+    expect(list1.concat(listOfLists)).toHaveValues(1, 2, 3, 4, 5, 6);
+  });
+});
 
-describe('filter list returning only values that satisfy the filter function', () => {
-  it('empty list', () => {
-    const list1 = List.create()
-    expect(list1.filter<number>((el) => el % 2 === 1)).toHaveValues()
-  })
+describe("filter list returning only values that satisfy the filter function", () => {
+  it("empty list", () => {
+    const list1 = List.create();
+    expect(list1.filter<number>((el) => el % 2 === 1)).toHaveValues();
+  });
 
-  it('non empty list', () => {
-    const list1 = List.create(1, 2, 3, 5)
-    expect(list1.filter<number>((el) => el % 2 === 1)).toHaveValues(1, 3, 5)
-  })
-})
+  it("non empty list", () => {
+    const list1 = List.create(1, 2, 3, 5);
+    expect(list1.filter<number>((el) => el % 2 === 1)).toHaveValues(1, 3, 5);
+  });
+});
 
-describe('returns the length of a list', () => {
-  it('empty list', () => {
-    const list1 = List.create()
-    expect(list1.length()).toEqual(0)
-  })
+describe("returns the length of a list", () => {
+  it("empty list", () => {
+    const list1 = List.create();
+    expect(list1.length()).toEqual(0);
+  });
 
-  it('non-empty list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    expect(list1.length()).toEqual(4)
-  })
-})
+  it("non-empty list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    expect(list1.length()).toEqual(4);
+  });
+});
 
-describe('returns a list of elements whose values equal the list value transformed by the mapping function', () => {
-  xit('empty list', () => {
-    const list1 = List.create()
-    expect(list1.map<number>((el) => ++el)).toHaveValues()
-  })
+describe("returns a list of elements whose values equal the list value transformed by the mapping function", () => {
+  xit("empty list", () => {
+    const list1 = List.create();
+    expect(list1.map<number>((el) => ++el)).toHaveValues();
+  });
 
-  xit('non-empty list', () => {
-    const list1 = List.create(1, 3, 5, 7)
-    expect(list1.map<number>((el) => ++el)).toHaveValues(2, 4, 6, 8)
-  })
-})
+  xit("non-empty list", () => {
+    const list1 = List.create(1, 3, 5, 7);
+    expect(list1.map<number>((el) => ++el)).toHaveValues(2, 4, 6, 8);
+  });
+});
 
-describe('folds (reduces) the given list from the left with a function', () => {
-  xit('empty list', () => {
-    const list1 = List.create()
-    expect(list1.foldl<number, number>((acc, el) => el * acc, 2)).toEqual(2)
-  })
+describe("folds (reduces) the given list from the left with a function", () => {
+  it("empty list", () => {
+    const list1 = List.create();
+    expect(list1.foldl<number, number>((acc, el) => el * acc, 2)).toEqual(2);
+  });
 
-  xit('direction independent function applied to non-empty list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    expect(list1.foldl<number, number>((acc, el) => acc + el, 5)).toEqual(15)
-  })
+  it("direction independent function applied to non-empty list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    expect(list1.foldl<number, number>((acc, el) => acc + el, 5)).toEqual(15);
+  });
 
-  xit('direction dependent function applied to non-empty list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    expect(list1.foldl<number, number>((acc, el) => el / acc, 24)).toEqual(64)
-  })
-})
+  it("direction dependent function applied to non-empty list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    expect(list1.foldl<number, number>((acc, el) => el / acc, 24)).toEqual(64);
+  });
+});
 
-describe('folds (reduces) the given list from the right with a function', () => {
-  xit('empty list', () => {
-    const list1 = List.create()
-    expect(list1.foldr<number, number>((acc, el) => el * acc, 2)).toEqual(2)
-  })
+describe("folds (reduces) the given list from the right with a function", () => {
+  xit("empty list", () => {
+    const list1 = List.create();
+    expect(list1.foldr<number, number>((acc, el) => el * acc, 2)).toEqual(2);
+  });
 
-  xit('direction independent function applied to non-empty list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    expect(list1.foldr<number, number>((acc, el) => acc + el, 5)).toEqual(15)
-  })
+  xit("direction independent function applied to non-empty list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    expect(list1.foldr<number, number>((acc, el) => acc + el, 5)).toEqual(15);
+  });
 
-  xit('direction dependent function applied to non-empty list', () => {
-    const list1 = List.create(1, 2, 3, 4)
-    expect(list1.foldr<number, number>((acc, el) => el / acc, 24)).toEqual(9)
-  })
-})
+  xit("direction dependent function applied to non-empty list", () => {
+    const list1 = List.create(1, 2, 3, 4);
+    expect(list1.foldr<number, number>((acc, el) => el / acc, 24)).toEqual(9);
+  });
+});
 
-describe('reverse the elements of a list', () => {
-  xit('empty list', () => {
-    const list1 = List.create()
-    expect(list1.reverse()).toHaveValues()
-  })
+describe("reverse the elements of a list", () => {
+  xit("empty list", () => {
+    const list1 = List.create();
+    expect(list1.reverse()).toHaveValues();
+  });
 
-  xit('non-empty list', () => {
-    const list1 = List.create(1, 3, 5, 7)
-    expect(list1.reverse()).toHaveValues(7, 5, 3, 1)
-  })
+  xit("non-empty list", () => {
+    const list1 = List.create(1, 3, 5, 7);
+    expect(list1.reverse()).toHaveValues(7, 5, 3, 1);
+  });
 
-  xit('list of lists is not flattened', () => {
-    const list1 = List.create([1, 2], [3], [], [4, 5, 6])
-    expect(list1.reverse()).toHaveValues([4, 5, 6], [], [3], [1, 2])
-  })
-})
+  xit("list of lists is not flattened", () => {
+    const list1 = List.create([1, 2], [3], [], [4, 5, 6]);
+    expect(list1.reverse()).toHaveValues([4, 5, 6], [], [3], [1, 2]);
+  });
+});
--- list-ops/list-ops.ts
+++ list-ops/list-ops.ts
@@ -1,55 +1,53 @@
 class Node {
-  _value : unknown;
-  _next : Node | null;
+  _value: unknown;
+  _next: Node | null;
 
-  constructor(value : unknown) {  
+  constructor(value: unknown) {
     this._value = value;
-    this._next = null
+    this._next = null;
   }
 
   // Setter functions
-  public set next(node : Node | null){
+  public set next(node: Node | null) {
     this._next = node;
   }
 
   // Getter functions
-  public get value() : unknown{
-    return this._value
+  public get value(): unknown {
+    return this._value;
   }
 
-  public get next() : Node | null {
-    return this._next
+  public get next(): Node | null {
+    return this._next;
   }
 }
 
 export class List {
-  _head : Node | null;
-  _tail : Node | null;
-  _length : number;
+  _head: Node | null;
+  _tail: Node | null;
+  _length: number;
 
-  constructor(values : unknown[]) {
-    let prev : Node | null = null;
+  constructor(values: unknown[]) {
+    let prev: Node | null = null;
     this._head = null;
     this._tail = null;
     this._length = 0;
 
-    values.forEach(element => {
-      if(element instanceof List){
+    values.forEach((element) => {
+      if (element instanceof List) {
         let newList = this.concat(element);
         this._head = newList.head;
         this._tail = newList.tail;
         this._length = newList.length();
-      }
-      else
-        this.push(element);  
+      } else this.push(element);
     });
   }
 
-  private push(value : unknown){
+  private push(value: unknown) {
     let node = new Node(value);
     this._length++;
 
-    if(this._head === null){
+    if (this._head === null) {
       this._head = node;
       this._tail = node;
       return;
@@ -59,75 +57,79 @@ export class List {
     this._tail = node;
   }
 
-  public forEach(callback : (item : unknown) => void){
+  public forEach(callback: (item: unknown) => void) {
     let ptr = this._head;
-    while(ptr !== null){
+    while (ptr !== null) {
       callback(ptr.value);
       ptr = ptr.next;
     }
+  }
 
-  };
-
-  public append(other : List) : List{
+  public append(other: List): List {
     let ptr = other.head;
 
-    while(ptr !== null){
+    while (ptr !== null) {
       this.push(ptr.value);
       ptr = ptr.next;
     }
-    return this
+    return this;
   }
 
-  public concat(other : List) : List{
+  public concat(other: List): List {
     var concatList = List.create();
     concatList.append(this);
     concatList.append(other);
     return concatList;
   }
 
-  public filter<T>(callback : (item : T) => boolean) : List{
+  public filter<T>(callback: (item: T) => boolean): List {
     let ptr = this._head;
     var newList = List.create();
 
-    while(ptr !== null){
-      if(callback(ptr.value as T))
-        newList.push(ptr.value);
-      
+    while (ptr !== null) {
+      if (callback(ptr.value as T)) newList.push(ptr.value);
+
       ptr = ptr.next;
     }
     return newList;
   }
 
-  public length() : number {
+  public length(): number {
     return this._length;
   }
 
-  public map<T>(callback : (item : T) => void){
+  public map<T>(callback: (item: T) => void) {
     throw new Error("Not Implemented");
   }
 
-  public foldl<T, R>(callback : (item1: T, item2: T) => T, constant : R){
-    throw new Error("Not Implemented");
+  public foldl<T, R>(callback: (item1: T, item2: T) => T, constant: T): T {
+    var acc: T = constant;
+    let ptr = this._head;
+    while (ptr !== null) {
+      acc = callback(acc, ptr.value as T);
+      ptr = ptr.next;
+    }
+    return acc;
   }
 
-  public foldr<T, R>(callback : (item1: T, item2: T) => T, constant : R){
+  public foldr<T, R>(callback: (item1: T, item2: T) => T, constant: R) {
     throw new Error("Not Implemented");
   }
 
   public reverse() {
-    let ptr = this._head
-    if(ptr !== null){
-      let values : typeof ptr.value[] = [];
+    let ptr = this._head;
+    if (ptr !== null) {
+      let values: typeof ptr.value[] = [];
       return new List(values);
     }
     return this;
   }
 
   // Getter implementation
-  public get head() : Node | null{
+  public get head(): Node | null {
     return this._head;
   }
-  public get tail() : Node | null{
+  public get tail(): Node | null {
     return this._tail;
   }
   public static create(...values: unknown[]): List {
@@ -136,6 +138,6 @@ export class List {
     // DO *not* use any of the Array.prototype methods in your solution.
 
     // You may use the destructuring and spreading (...) syntax from Iterable.
-    return new List(values)
+    return new List(values);
   }
 }
