bc22538dce171f400367d7313239c2968b8275d1
Najeeb Khan <najeeb.khan@moia.io>
2024-05-05 13:40:16 +0200

    improve leap year solution

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,13 +1,4 @@
 object Leap {
   def leapYear(year: Int): Boolean =
-    (year % 4) match {
-      case 0 => (year % 100) match {
-        case 0 => (year % 400) match {
-          case 0 => true
-          case _ => false
-        }
-        case _ => true 
-      }
-      case _ => false
-    }
+    (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
 }
