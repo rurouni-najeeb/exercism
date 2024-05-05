58682c998e0013347516c41b1d27485bc5558be3
Najeeb Khan <najeeb.khan@moia.io>
2024-05-05 14:18:03 +0200

    implement curried solution

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -15,3 +15,17 @@ object SpaceAge {
 
   def onNeptune(age: Double): Double = onEarth(age) / 164.79132
 }
+
+object SpaceAgeCurry {
+  private val EARTH_SECONDS = 31_557_600.0
+  private def calculate(orbitPeriod: Double) = (seconds : Double) => seconds / EARTH_SECONDS / orbitPeriod
+
+  val onVenus = calculate(0.61519726)
+  val onEarth = calculate(1)
+  val onMercury = calculate(0.2408467)
+  val onMars = calculate(1.8808158)
+  val onJupiter = calculate(11.862615)
+  val onSaturn = calculate(29.447498)
+  val onUranus = calculate(84.016846)
+  val onNeptune = calculate(164.79132)
+}
