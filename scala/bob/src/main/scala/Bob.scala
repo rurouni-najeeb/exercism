4a3ebdc79c19dc0dbc94f1628af92d1b0a40ed85
Najeeb Khan <najeeb.khan@moia.io>
2024-05-12 17:21:29 +0200

    implement bob solution

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,20 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String = {
+    statement.trim() match {
+      case s if s.isBlank() => "Fine. Be that way!"
+      case s if isQuestion(s) && isYelling(s) =>
+        "Calm down, I know what I'm doing!"
+      case s if isQuestion(s) => "Sure."
+      case s if isYelling(s)  => "Whoa, chill out!"
+      case _                  => "Whatever."
+    }
+  }
+
+  def isQuestion(statement: String): Boolean = statement.endsWith("?")
+  def isYelling(statement: String): Boolean = {
+    val statementWithOnlyAlphabets = statement.replaceAll("[^a-zA-Z]", "")
+    !statementWithOnlyAlphabets
+      .isBlank() && (statementWithOnlyAlphabets
+      .toUpperCase() == statementWithOnlyAlphabets)
+  }
 }
