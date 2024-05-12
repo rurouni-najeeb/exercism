6ff354c83c6ad95b3b6b69f5367a205d1c5e0984
Najeeb Khan <85175973+rurouni-najeeb@users.noreply.github.com>
2024-05-12 18:08:47 +0200

    feat(scala): improve solution by not calling function multiple times (#10)

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,20 +1,19 @@
 object Bob {
   def response(statement: String): String = {
-    statement.trim() match {
-      case s if s.isBlank() => "Fine. Be that way!"
-      case s if isQuestion(s) && isYelling(s) =>
-        "Calm down, I know what I'm doing!"
-      case s if isQuestion(s) => "Sure."
-      case s if isYelling(s)  => "Whoa, chill out!"
-      case _                  => "Whatever."
-    }
-  }
+    val trimmedStatement = statement.trim()
 
-  def isQuestion(statement: String): Boolean = statement.endsWith("?")
-  def isYelling(statement: String): Boolean = {
-    val statementWithOnlyAlphabets = statement.replaceAll("[^a-zA-Z]", "")
-    !statementWithOnlyAlphabets
-      .isBlank() && (statementWithOnlyAlphabets
-      .toUpperCase() == statementWithOnlyAlphabets)
+    if (trimmedStatement.isEmpty()) return "Fine. Be that way!"
+
+    val isQuestion: Boolean = trimmedStatement.endsWith("?")
+    val isYelling: Boolean = trimmedStatement.exists(
+      _.isLetter
+    ) && trimmedStatement.toUpperCase() == statement
+
+    (isYelling, isQuestion) match {
+      case (true, true) => "Calm down, I know what I'm doing!"
+      case (_, true)    => "Sure."
+      case (true, _)    => "Whoa, chill out!"
+      case _            => "Whatever."
+    }
   }
 }
